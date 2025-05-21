a75877279764725c225b95d674ea3a6d0f6ac6d6
Esmoris02 <matias.a.esmoris@gmail.com>
2025-05-21 08:55:59 -0300

    Corrección Cat Soc
    Se añadió el schema en la cración de la tabla y se agrego un dropp por si es necesario
    
    Creación BD
    
    Se crea la base de datos el schema y una tabla

---
+++ DBSolNorte.sql
@@ -0,0 +1,16 @@
+create database ClubSolNorte
+go
+use ClubSolNorte
+go
+create schema dbsl
+--data base sol norte
+go
+CREATE TABLE dbsl.CategoriaSocio (
+    id_categoria INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    edad_desde INT,
+    edad_hasta INT
+);
+go
+--drop table dbsl.CategoriaSocio
+
