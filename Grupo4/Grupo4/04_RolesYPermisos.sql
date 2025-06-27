6aab223557fcc55129099e4a78846b759643aab8
Bruno Morales Pessacq <brunomp567@gmail.com>
2025-06-27 11:19:52 -0300

    RolesYPermisos Con Juegodeprueba

--- Grupo4/Grupo4/04_RolesYPermisos.sql
+++ Grupo4/Grupo4/04_RolesYPermisos.sql
@@ -6,52 +6,53 @@ CREATE LOGIN login_adminClub
 WITH PASSWORD = '#SN012025ad',
      CHECK_POLICY = ON;
 
-CREATE LOGIN login_jefeDeTesoreria WITH PASSWORD = '#SN012025a'
-	MUST_CHANGE, 
+CREATE LOGIN login_jefeDeTesoreria 
+WITH PASSWORD = '#SN012025a',
+     DEFAULT_DATABASE = ClubSolNorte,
+     CHECK_POLICY = ON;
+
+CREATE LOGIN login_administrativoCobranza
+WITH PASSWORD = '#SN012025b',
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_administrativoCobranza WITH PASSWORD = '#SN012025b'
-	MUST_CHANGE, 
+CREATE LOGIN login_administrativoMorosidad 
+WITH PASSWORD = '#SN012025c',
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_administrativoMorosidad WITH PASSWORD = '#SN012025c'
-	MUST_CHANGE, 
+CREATE LOGIN login_administrativoFacturacion
+WITH PASSWORD = '#SN012025d',
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_administrativoFacturacion WITH PASSWORD = '#SN012025d'
-	MUST_CHANGE, 
+CREATE LOGIN login_administrativoSocio 
+WITH PASSWORD = '#SN012025e',
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_administrativoSocio WITH PASSWORD = '#SN012025e'
-	MUST_CHANGE, 
-    DEFAULT_DATABASE = ClubSolNorte, 
-    CHECK_POLICY = ON;
-
-CREATE LOGIN login_socioWeb WITH PASSWORD = '#SN012025f'
+CREATE LOGIN login_socioWeb
+WITH PASSWORD = '#SN012025f',
 	DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_presidente WITH PASSWORD = '#SN012025g'
-	MUST_CHANGE, 
+CREATE LOGIN login_presidente
+WITH PASSWORD = '#SN012025g', 
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_vicepresidente WITH PASSWORD = '#SN012025h'
-	MUST_CHANGE, 
+CREATE LOGIN login_vicepresidente
+WITH PASSWORD = '#SN012025h',
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_secretario WITH PASSWORD = '#SN012025i'
-	MUST_CHANGE, 
+CREATE LOGIN login_secretario
+WITH PASSWORD = '#SN012025i',
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
-CREATE LOGIN login_vocales WITH PASSWORD = '#SN012025j'
-	MUST_CHANGE, 
+CREATE LOGIN login_vocales
+WITH PASSWORD = '#SN012025j',
     DEFAULT_DATABASE = ClubSolNorte, 
     CHECK_POLICY = ON;
 
@@ -185,12 +186,100 @@ BEGIN
    
     DECLARE @ContraseniaEncriptada VARBINARY(256) = ENCRYPTBYKEY(KEY_GUID('SN_DatosSensibles'), @Contrasenia);
 	DECLARE @DireccionEncriptada VARBINARY(256) = ENCRYPTBYKEY(KEY_GUID('SN_DatosSensibles'), @Direccion);
-	DECLARE @FecNacEncriptada VARBINARY(256) = ENCRYPTBYKEY(KEY_GUID('SN_DatosSensibles'), CONVERT((NVARCHAR(30)),@FecNac));
+	DECLARE @FecNacEncriptada VARBINARY(256) = ENCRYPTBYKEY(KEY_GUID('SN_DatosSensibles'), CONVERT(NVARCHAR(30),@FecNac));
 
     CLOSE SYMMETRIC KEY SN_DatosSensibles;
  
 	INSERT INTO dbsl.Usuario(Usuario,Estado,Contrasenia,Rol,FecVig,NroSocio,Direccion,FecNac)
 	VALUES (@Usuario, @Estado, @ContraseniaEncriptada, @Rol, @FecVig, @NroSocio, @DireccionEncriptada, @FecNacEncriptada)
 END
+
+EXEC dbsl.insertarUsuario 
+    @Usuario = 'admin1',
+    @Estado = 'activo',
+    @Contrasenia = 'AdminPass123',
+    @Rol = 'administrador',
+    @FecVig = '2025-06-01',
+    @Direccion = 'Av. Siempre Viva 123',
+    @FecNac = '1990-04-10',
+    @NroSocio = NULL;
+
+EXEC dbsl.insertarUsuario 
+    @Usuario = 'profesor_maria',
+    @Estado = 'activo',
+    @Contrasenia = 'ClaseFuerte456',
+    @Rol = 'profesor',
+    @FecVig = '2025-06-01',
+    @Direccion = 'Calle 9 de Julio 200',
+    @FecNac = '1985-11-15',
+    @NroSocio = NULL;
+
+EXEC dbsl.insertarUsuario 
+    @Usuario = 'socio_juan',
+    @Estado = 'activo',
+    @Contrasenia = 'Socio2025',
+    @Rol = 'socio',
+    @FecVig = '2025-06-01',
+    @Direccion = 'Ruta 3 Km 15',
+    @FecNac = '2002-03-12',
+    @NroSocio = NULL;
+
+EXEC dbsl.insertarUsuario 
+    @Usuario = 'socio_ana',
+    @Estado = 'activo',
+    @Contrasenia = 'AnaClave789',
+    @Rol = 'socio',
+    @FecVig = '2025-06-01',
+    @Direccion = 'Av. Mitre 456',
+    @FecNac = '2006-08-05',
+    @NroSocio = NULL;
+
+EXEC dbsl.insertarUsuario 
+    @Usuario = 'admin2',
+    @Estado = 'inactivo',
+    @Contrasenia = 'NoDisponible1',
+    @Rol = 'administrador',
+    @FecVig = '2024-12-15',
+    @Direccion = 'Diagonal Norte 10',
+    @FecNac = '1980-02-20',
+    @NroSocio = NULL;
+
+EXEC dbsl.insertarUsuario 
+    @Usuario = 'profe_jose',
+    @Estado = 'activo',
+    @Contrasenia = 'Clase123!',
+    @Rol = 'profesor',
+    @FecVig = '2025-01-01',
+    @Direccion = 'Santa Fe 1050',
+    @FecNac = '1992-07-30',
+    @NroSocio = NULL;
+
+EXEC dbsl.insertarUsuario 
+    @Usuario = 'socio_lucia',
+    @Estado = 'activo',
+    @Contrasenia = 'LuciaSegura',
+    @Rol = 'socio',
+    @FecVig = '2025-06-01',
+    @Direccion = 'Belgrano 300',
+    @FecNac = '2010-09-12',
+    @NroSocio = NULL;
+
+
+
+OPEN SYMMETRIC KEY SN_DatosSensibles
+DECRYPTION BY CERTIFICATE Cert_ClubSolNorte;
+
+SELECT Usuario,
+       CONVERT(VARCHAR, DecryptByKey(Direccion)) AS DireccionDesencriptado,
+	   CONVERT(DATE, CONVERT(NVARCHAR(30), DECRYPTBYKEY(FecNac))) AS FechaNacimiento
+FROM dbsl.Usuario;
+
+CLOSE SYMMETRIC KEY SN_DatosSensibles;
+
+
+
+SELECT Usuario,Direccion,FecNac
+FROM dbsl.Usuario;
+
 
 
